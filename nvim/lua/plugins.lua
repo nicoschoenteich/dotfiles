6081c2cf6379e4d747ad95c653d5d13cf00b53b1
nicoschoenteich <nicolai.schoenteich@gmail.com>
2023-05-03 11:17:32 +0200

    configure kitty

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,8 @@
+-- required steps:
 -- git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 -- :PackerInstall
 -- :PackerCompile
+-- make sure to install https://github.com/ryanoasis/nerd-fonts
 
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
