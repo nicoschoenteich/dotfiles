c3397241dc8e354aa8942849f05c4c21c3ee61c5
nicoschoenteich <nicolai.schoenteich@sap.com>
2024-04-26 14:58:11 +0200

    new statusline with parent dir of file

--- nvim/lua/statusline.lua
+++ nvim/lua/statusline.lua
@@ -1,21 +1,11 @@
 local function status_line()
-
-	-- local file_name = "%t"
-	-- local right_align = "%="
-	-- local line_no = "%10([%l/%L%)]"
-
-	-- return string.format(
-    	-- "%s%s%s",
-	-- 	right_align,
-	-- 	file_name,
-	-- 	line_no
-	-- )
-
-	return "%=%t"
-
+	local rightAlign = "%="
+	local parentDir = "%{expand('%:p:h:t')}"
+	local currentFile = "%t"
+	return rightAlign .. "... " .. parentDir .. "/" .. currentFile
 end
 
 vim.opt.statusline = status_line()
 
 -- only show most recent status line (think of multiple windows)
-vim.opt.laststatus=3
+vim.opt.laststatus = 3
